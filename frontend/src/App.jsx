8e4827acf0a4727f14d191135e298dd959d3115a
Paigomanh <rahulrahulsajeevan007@gmail.com>
2025-02-10 11:14:46 +0530

    added uselocation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,28 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
-import Market from './Pages/Market';
-import Docs from './Pages/Docs';
-import Navbar from './Component/Navbar';
-import Governance from './Pages/Governance/App';
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./Pages/Home";
+import Market from "./Pages/Market";
+import Docs from "./Pages/Docs";
+import Navbar from "./Component/Navbar";
+import Governance from "./Pages/Governance/App";
 
 const App = () => {
-  return (
-    <div className='bg-black text-white w-screen h-screen'>
-    <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/governance/*" element={<Governance />} />
-        <Route path="/market" element={<Market />} />
-        <Route path="/docs" element={<Docs />} />
-      </Routes>
-    </div>
-  );
-}
+    const location = useLocation();
+    
+    const hideNavbarRoutes = ["/governance"];
+
+    return (
+        <div className="bg-black text-white w-screen h-screen">
+          
+            {!hideNavbarRoutes.some(route => location.pathname.startsWith(route)) && <Navbar />}
+            
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/governance/*" element={<Governance />} />
+                <Route path="/market" element={<Market />} />
+                <Route path="/docs" element={<Docs />} />
+            </Routes>
+        </div>
+    );
+};
 
 export default App;
